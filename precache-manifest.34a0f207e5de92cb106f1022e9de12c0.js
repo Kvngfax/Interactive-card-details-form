6f60d6d0c240a3092c1d7214aee96058a0c7a8a4
Abdulbasit Fasasi <kvngfax23@gmail.com>
2023-06-17 10:21:39 +0100

    Updates

---
+++ precache-manifest.34a0f207e5de92cb106f1022e9de12c0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "51a38a75604e6f1d0a6b",
+    "url": "/please/static/css/main.2bb3bcdb.chunk.css"
+  },
+  {
+    "revision": "51a38a75604e6f1d0a6b",
+    "url": "/please/static/js/main.51a38a75.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/please/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "288be7b24413fee0738e",
+    "url": "/please/static/js/2.288be7b2.chunk.js"
+  },
+  {
+    "revision": "62f7c4a309d7e2d1c67b",
+    "url": "/please/static/js/runtime~main.62f7c4a3.js"
+  },
+  {
+    "revision": "9f0cf5fb7ecac9ee2ff80073b42247ff",
+    "url": "/please/static/media/card-logo.9f0cf5fb.svg"
+  },
+  {
+    "revision": "530df842c362d952de57b6d262361efc",
+    "url": "/please/static/media/icon-complete.530df842.svg"
+  },
+  {
+    "revision": "66d8eb6b6b107baf8b8abf4dbecb4e07",
+    "url": "/please/static/media/bg-main-desktop.66d8eb6b.png"
+  },
+  {
+    "revision": "33d6bbef0946f7071752c239b14d4a67",
+    "url": "/please/static/media/bg-card-front.33d6bbef.png"
+  },
+  {
+    "revision": "1a41a92ca7b04ab1362f1b2f6bb2a83a",
+    "url": "/please/static/media/bg-card-back.1a41a92c.png"
+  },
+  {
+    "revision": "3b8e5db1428efb79aa0006dfbef1d690",
+    "url": "/please/static/media/bg-main-mobile.3b8e5db1.png"
+  },
+  {
+    "revision": "18fb66be2c816383831982c7f194c64f",
+    "url": "/please/index.html"
+  }
+];
